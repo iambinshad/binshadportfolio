57e7fc70ace2dedca34b96fdcea3602f50db389f
iambinshad <binshadp999@gmail.com>
2024-09-24 15:08:08 +0530

    Ui changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1635570736"
+    serviceWorkerVersion: "1265798852"
   }
 });
